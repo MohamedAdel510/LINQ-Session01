8341248918997b72f9da08df8ad42b05d1178310
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-08-06 03:03:14 +0300

    Add project files.

---
+++ Asssignment/Program.cs
@@ -0,0 +1,196 @@
+﻿using System.Threading;
+using static Asssignment.ListGenerator;
+namespace Asssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Restriction Operators [where]
+
+            #region Q1 Find all products that are out of stock.
+
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0);
+
+            #endregion
+
+            #region Q2 Find all products that are in stock and cost more than 3.00 per unit
+
+            //var Result = ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3.00M);
+
+            #endregion
+
+            #region Q3 Returns digits whose name is shorter than their value.
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var Result = Arr.Where((E, I) => E.Length < I);
+            #endregion
+
+            #endregion
+
+            #region Element Operators
+
+            #region Q1 Get first Product out of Stock 
+
+            //var Result = ProductsList.First(P => P.UnitsInStock == 0);
+            //Console.WriteLine(Result);
+
+            #endregion
+
+            #region Q2 Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            //var Result = ProductsList.FirstOrDefault(P => P.UnitPrice > 1000);
+            //Console.WriteLine(Result?.ProductName ?? "Not Found");
+
+            #endregion
+
+            #region Q3 Retrieve the second number greater than 5 
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var first = Arr.FirstOrDefault(P => P > 5);
+            //var second = Arr.FirstOrDefault(P => P > 5 && P != first);
+            //Console.WriteLine(second.ToString() ?? "Not found");
+
+            #endregion
+
+            #endregion
+
+            #region Ordering Operators
+
+            #region Q1 Sort a list of products by name
+
+            //var Result = ProductsList.OrderBy(P => P.ProductName);
+
+            #endregion
+
+            #region Q2 Uses a custom comparer to do a case-insensitive sort of the words in an array.
+
+            //string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var Result = Arr.OrderBy(x => x);
+
+            #endregion
+
+            #region Q3 Sort a list of products by units in stock from highest to lowest.
+
+            //var Result = ProductsList.OrderByDescending(P => P.UnitsInStock);
+
+            #endregion
+
+            #region Q4 Sort a list of digits, first by length of their name, and then alphabetically by the name itself.
+
+            //string[] Arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            //var Result = Arr.OrderBy(E => E.Length).ThenBy(E => E);
+
+            #endregion
+
+            #region Q5  Sort first by-word length and then by a case-insensitive sort of the words in an array.
+
+            //string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var Result = Arr.OrderBy(E => E.Length).ThenBy(E => E);
+
+            #endregion
+
+            #region Q6  Sort a list of products, first by category, and then by unit price, from highest to lowest.
+
+            //var Result = ProductsList.OrderBy(P => P.Category).ThenByDescending(P => P.UnitPrice);
+
+            #endregion
+
+            #region Q7 Sort first by-word length and then by a case-insensitive descending sort of the words in an array.
+
+            //string[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var Result = Arr.OrderBy( E => E.Length).ThenByDescending( E => E );
+
+            #endregion
+
+            #region Q8 Create a list of all digits in the array whose second letter is 'i' that is reversed from the order in the original array.
+
+            //string[] Arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            //var Result = Arr.Where(E => E[1] == 'i').Reverse();
+
+            #endregion
+
+            #endregion
+
+            #region Transformation Operators
+
+            #region Q1 Return a sequence of just the names of a list of products
+
+            //var Result = ProductsList.Select(P => P.ProductName);
+
+            #endregion
+
+            #region Q2 Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
+
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            //var Result = words.Select(W => new
+            //{
+            //    L = W.ToLower(),
+            //    U = W.ToUpper(),
+            //});
+            //foreach (var Item in Result)
+            //{
+            //    Console.WriteLine($"{Item.L} , {Item.U}");
+            //}
+
+            #endregion
+
+            #region Q3 Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type.
+
+            //var Result = ProductsList.Select(P => new
+            //{
+            //    PId = P.ProductID,
+            //    PName = P.ProductName,
+            //    Price = P.UnitPrice
+            //});
+
+            #endregion
+
+            #region Q4 Determine if the value of int in an array matches their position in the array.
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Select((E, I) => new
+            //{
+            //    Number = E,
+            //    InPlace = E == I
+            //});
+
+            //Console.WriteLine("Number: In-Place?");
+            //foreach (var Item in Result)
+            //{
+            //    Console.WriteLine($"{Item.Number}: {Item.InPlace}");
+            //}
+
+            #endregion
+
+            #region Q5 Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            #endregion
+
+            #region Q6 Select all orders where the order total is less than 500.00.
+
+            //var Result = CustomersList.SelectMany(C => C.Orders).Where(O => O.Total < 500.00M);
+
+            #endregion
+
+            #region Q7 Select all orders where the order was made in 1998 or later
+
+            //var Result = CustomersList.SelectMany(C => C.Orders).Where(O => O.OrderDate.Year == 1998);
+
+            #endregion
+
+            #endregion
+
+
+            //foreach (var Item in Result)
+            //{
+            //    Console.WriteLine(Item);
+            //}
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,244 @@
+﻿using static Demo.ListGenerator;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            #region What is Linq
+
+            //1- Local sequance (static).
+            //Predicate<int> prd = (x) => { return x % 2 == 0; };
+            //List<int> OddNumbers = Numbers.Where(X => X % 2 == 1).ToList();
+
+            //foreach (int num in OddNumbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+            #endregion
+
+            #region LINQ Syntax
+
+            #region Fluent syntax
+            ////1 - Call "Linq operator" as static method 
+            //var OddNumbers = Enumerable.Where(Numbers, X => X % 2 == 1);
+
+            ////2 - call "Linq operstor" as extintion method {recomended}
+            //var OddNumbers = Numbers.Where(X => X % 2 == 1);
+
+            //foreach (var number in OddNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
+            #region Query syntax
+            //var OddNumbers = from N in Numbers
+            //                 where N % 2 == 1
+            //                 select N;
+
+            //foreach (var number in OddNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
+            #endregion
+
+            #region Linq execution
+            //var OddNumbers = Numbers.Where(X => X % 2 == 1).ToList(); // Where Executed with deferred execution
+
+            //Numbers.AddRange(new[] { 11, 12, 13, 14, 15 }); 
+            //foreach (var number in OddNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
+            #region Data Setup
+            //Console.WriteLine(ProductsList[0]);
+            //Console.WriteLine(CustomersList[0]);
+            #endregion
+
+            #region Linq operators
+
+            #region Filtration [Restriction] Operator - Where
+            #region Get Elements Out Of Stock
+            // Fluent
+            //var Result = ProductsList.Where(p => p.UnitsInStock == 0);
+
+            //Query
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock == 0
+            //             select P;
+            #endregion
+
+            #region Get Elements In Stock And In Category Of Meat/Poultry 
+            ////Fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0 && P.Category == "Meat/Poultry");
+
+            //// Query
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 0 && P.Category == "Meat/Poultry"
+            //             select P;
+            #endregion
+
+            #region Get Elements   Out Of Stock But In 10 Elements
+            //// We need to use indexed where only valid with fluent syantax
+            //var Result = ProductsList.Where((P, I) => I < 10 && P.UnitsInStock == 0);
+            #endregion
+
+            #endregion
+
+            #region Transformation [Projection] Operators [Select , Select Many]
+
+            #region Select Product Name
+            //// Fluent 
+            //var Result = ProductsList.Select(P => P.ProductName);
+
+            //// Query
+            //var Result = from P in ProductsList
+            //             select P.ProductName;
+            #endregion
+
+            #region Select Customer Name
+            //// Fluent 
+            //var Result = CustomersList.Select(C => C.CustomerName);
+
+            ////Query 
+            //var Result = from C in CustomersList
+            //             select C.CustomerName;
+            #endregion
+
+            #region Select Customer Orders
+            ////Fluent
+            //var Result = CustomersList.SelectMany(C => C.Orders);
+
+            ////Query
+            //var Result = from C in CustomersList
+            //             from O in C.Orders
+            //             select O;
+            #endregion
+
+            #region Select Product Id and Product Name 
+            ////Fluent
+            //var Result = ProductsList.Select(P => new  { ProductID = P.ProductID, ProductName = P.ProductName });
+
+            ////Query
+            //var Result = from P in ProductsList
+            //             select new
+            //             {
+            //                 PId = P.ProductID,
+            //                 PName = P.ProductName,
+            //             };
+            #endregion
+
+            #region Select Product In Stock And Apply Discount 10 % On Its Price
+            //// Fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0)
+            //    .Select(P => new
+            //    {
+            //PId = P.ProductID,
+            //PName = P.ProductName,
+            //OldPrice = P.UnitPrice,
+            //NewPrice = P.UnitPrice * .9m
+            //    });
+
+            //// Query 
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 0
+            //             select new
+            //             {
+            //                 PId = P.ProductID,
+            //                 PName = P.ProductName,
+            //                 OldPrice = P.UnitPrice,
+            //                 NewPrice = P.UnitPrice * .9m
+            //             };
+            #endregion
+
+
+            #region Indexed Select
+            // Vaild only with fluent syntax
+            //var Result = ProductsList.Select((P, I) => new
+            //    {
+            //        Index = I,
+            //        PName = P.ProductName,
+            //    });
+            #endregion
+
+            #endregion
+
+            #region Ording Operators [Ascending , Descending , Reverse , ThenBy , ThenByDescending]
+            #region Get Products Ordered By Price Asc
+            ////Fluent
+            //var Result = ProductsList.OrderBy(P => P.UnitPrice);
+
+            ////Query
+            //var Result = from P in ProductsList
+            //             orderby P.UnitPrice
+            //             select P;
+            #endregion
+
+            #region Get Products Ordered By Price Desc
+            ////Fluent
+            //var Result = ProductsList.OrderByDescending(P => P.UnitPrice);
+
+            ////Query
+            //var Result = from P in ProductsList
+            //             orderby P.UnitPrice descending
+            //             select P;
+            #endregion
+
+            #region Get Products Ordered By Price Asc and Number Of Items In Stock
+            //var Result = ProductsList.OrderBy(P => P.UnitPrice).ThenByDescending(P => P.ProductName);
+
+            #endregion
+
+            #region Reverse
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0).Reverse();
+            #endregion
+            #endregion
+
+            //foreach (var Unit in Result)
+            //{
+            //    Console.WriteLine(Unit);
+            //}
+
+            #region Elements Operator - Immediate Execution [Valid Only With Fluent Syntax]
+
+            #region [First - Last - FirstOrDefault - LastOrDefault]
+            //var Result = ProductsList.First(); // Get First Element
+            //Result = ProductsList.First(P => P.UnitsInStock == 0); // get first element match the conadition
+
+            //Result = ProductsList.Last();// Last Element , 
+            //Result = ProductsList.Last(P => P.UnitsInStock == 0);// Last Element Matches the Condition
+            ////them all Throw exception if there is no element Found
+            //// we use OrDefult To Handle it
+
+            //Result = ProductsList.FirstOrDefault();
+            //Result = ProductsList.LastOrDefault();
+            #endregion
+
+            #region [ ElementAt - ElementAtOrDefault ]
+            //var Result = ProductsList.ElementAt(1);  
+            //// Result = ProductsList.ElementAt(999);   // Will Throw Exception 
+            //Result = ProductsList.ElementAtOrDefault(999);  // To Handel The Exception 
+            #endregion
+
+            #region [Single - SingleOrDefault]
+            ////var Result = ProductsList.Single(P => P.UnitsInStock == 0); // Will Throw Exception 
+            //// if there are many product Mathes the Condition or if there is no product
+
+            var Result = ProductsList.SingleOrDefault(P => P.UnitsInStock == 0); 
+            #endregion
+
+            Console.WriteLine(Result?.ProductName ?? "Not Found");
+            #endregion
+            #endregion
+
+
+        }
+    }
+}
